33e3ebee7102d6b25fcc23439659a85508141dcc
angryhors <groznykon@gmail.com>
2020-02-06 18:08:51 +0100

    fixed a lot of stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    Transform mainCamera;
+    public float moveSpeed = 5;
+    public float unwindingSpeed = 0.001f;
+    public static int livesCount = 3;
+    public Material[] playerMaterials = new Material[3];
+
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public MazeGenerator mazeGeneratorScript;
+    public GameObject maze;
+    public MeshRenderer meshRenderer;
+    public Player playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+        rb = this.GetComponent<Rigidbody>();
+        GameObject maze = GameObject.Find("Maze");
+        mazeGeneratorScript = maze.GetComponent<MazeGenerator>();
+        GetComponent<Renderer>().material = playerMaterials[livesCount-1];
+        foreach (Transform child in transform) {
+            if (child.gameObject.name == "Trail") {
+                child.GetComponent<TrailRenderer>().material = playerMaterials[livesCount-1];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero) {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            targetRotation += mainCamera.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            rb.MovePosition(transform.position + transform.forward * moveSpeed * Time.deltaTime);
+            transform.localScale -= Vector3.one * unwindingSpeed * Time.deltaTime;
+        }
+
+        if (transform.localScale.x <= 0.1) {
+            foreach (Transform child in transform) {
+                if (child.gameObject.name == "Main Camera") {
+                    GameObject.Destroy(child.gameObject);
+                }
+            }
+            meshRenderer.enabled = false;
+            playerScript.enabled = false;
+            livesCount--;
+
+            if (livesCount > 0) {
+                mazeGeneratorScript.PutPlayerOnStartingPosition();
+                GetComponent<Renderer>().material = playerMaterials[livesCount-1];
+                foreach (Transform child in transform) {
+                    if (child.gameObject.name == "Trail") {
+                        child.GetComponent<TrailRenderer>().material = playerMaterials[livesCount];
+                    }
+                }
+            }  else {
+                Application.Quit();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Destination(Clone)") {
+            StartCoroutine(ExecuteAfterTime(1));
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    float yaw;
+    float pitch;
+
+    public Transform target;
+    public float mouseSensitivity = 10f;
+    public Vector2 minMaxDistanceFromTarget = new Vector2(0.5f, 2f);
+    public Vector2 pitchMinMax = new Vector2(0, 45);
+
+    public float rotationSmoothTime = 0.05f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    public float cameraDistanceSmoothTime = 0.1f;
+    float cameraDistanceSmoothVelocity;
+    float cameraDistance;
+
+    public bool lockCursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 currentRotation = transform.eulerAngles;
+        if (lockCursor) {
+          Cursor.lockState = CursorLockMode.Locked;
+          Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.forward, out hit)) {
+            float desiredCameraDistance = Mathf.Clamp(hit.distance * 0.9f, minMaxDistanceFromTarget.x, minMaxDistanceFromTarget.y);
+            float playerCameraDist = Vector3.Distance(transform.position, target.position);
+            cameraDistance = Mathf.SmoothDamp(playerCameraDist, desiredCameraDistance, ref cameraDistanceSmoothVelocity, cameraDistanceSmoothTime);
+        }
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+        transform.position = target.position - transform.forward * cameraDistance;
+    }
+}
